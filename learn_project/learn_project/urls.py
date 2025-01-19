a30851925d8972abea40eaf9ad7808a4ecb40148
mms212187 <mms2107@gmail.com>
2025-01-19 12:55:41 +1000

    Delete learn_project directory

--- learn_project/learn_project/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('learn_app.urls')),
-    path('accounts/', include('allauth.urls')),
-    path('social-auth/', include('social_django.urls')),
-]
