fb77452f46a902c82064872cb5d860aadee6a489
mms2187 <mms2107@gmail.com>
2025-01-21 18:55:07 +1000

    проджект менеджмент

--- project_management/project_management/settings.py
+++ project_management/project_management/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-sw9l=7myq8jn7a5l^15!n7zlvqu1b5%ovml1%-hjgrgq50ugc^'
+SECRET_KEY = 'django-insecure-jr(ew9ahgi%dp=gdhb-8@6n-2^8t#psmuj=9ars8rlhc=rrxon'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
